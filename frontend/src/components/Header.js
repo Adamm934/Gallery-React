1450da07e579e3e5d8a79c38ef8d027fe99f13bb
Adam Malinovski <Adam Malinovski>
2022-01-08 15:42:14 +0000

    Managed to attack event tu button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Navbar, NavbarBrand } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+
+const navbarStyle = {
+    backgroundColor: 'rgba(229, 213, 255, 0.3)'
+};
 
 const Header = ({title}) => {
   return (
-    <Navbar bg = "light" variant = "light">
-        <Navbar.Brand href = "/">{title}</Navbar.Brand>
+    <Navbar style={navbarStyle}  variant = "light">
+        <Container>
+            <Navbar.Brand href = "/">{title}</Navbar.Brand>
+            
+        </Container>
+        
     </Navbar>
   )
 };
